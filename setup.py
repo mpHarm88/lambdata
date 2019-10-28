c584f5e95d267b016722fac031087d9e0e124bc6
mpHarm88 <mpHarm88@users.noreply.github.com>
2019-10-28 11:03:01 -0700

    added setup.py file and upload to pypi

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+Lambdata - a collection of data science helper finctions for lambda school
+"""
+
+import setuptools
+
+REQUIRED = [
+        "numpy",
+        "pandas"
+        ]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTON = fh.read()
+    setuptools.setup(
+            name="lambdata-mpharm88",
+            version="0.1.0",
+            author="mpharm88",
+            description="a collection of data science helper functions",
+            long_description=LONG_DESCRIPTON,
+            long_description_content_type="text/markdown",
+            url="https://github.com/mpHarm88/lambdata",
+            packages=setuptools.find_packages(),
+            python_requires=">=3.5",
+            install_requires=REQUIRED,
+            classifiers=["Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+             ]
+            )
