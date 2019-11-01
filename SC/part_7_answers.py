5bd27d59ce630f13387b3f5ac46c36ba27e5dd22
mpHarm88 <mpHarm88@users.noreply.github.com>
2019-11-01 13:39:24 -0700

    unit 3 sprint 1 submission

---
+++ SC/part_7_answers.py
@@ -0,0 +1,22 @@
+"""
+- What, in your opinion, is an important part of code reviews? That is, what is
+  something you pay attention to when you review code, and that you appreciate
+  when others do the same for your code?
+
+  ANSWER: An important part of code reviews is providing both negative and
+  positive feedback. It's hard for anyone to only recieve negative feedback on 
+  everyting they do and this is why its important to also mention the positives 
+  of their work too. Readability is also important and I appreciate when others
+  tell me my code is unreadable. I also enjoy when others tell how I can 
+  refactor my code when it was not obvious to myself.
+
+  - We have an awful lot of computers here, and it gets pretty confusing with
+  slightly different things running on all of them. How could containers help
+  us improve this situation?
+
+  ANSWER: Using a container would help the above described situation by letting
+  the server  operator run all of their "slightly different things" in one 
+  virtual enviroment as images. This way you could manage all of your computer 
+  enviroments from a single location while still letting all of them have the 
+  nuances that existed previously.
+"""
